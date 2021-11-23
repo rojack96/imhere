31597c8e9f08f09c75ce02fac9fd6ee75d5df7bd
Rojack96 <cerroberto96@gmail.com>
2021-11-23 06:56:42 +0100

    add line of code in print

--- imhere/delogger/delogger.py
+++ imhere/delogger/delogger.py
@@ -3,6 +3,5 @@ import inspect
 
 class Delogger:
 
-    def return_name(counter=[0]):
-        counter[0]+=1 # mutable variable get evaluated ONCE
-        return print(inspect.stack()[-1][1] + "_" + inspect.stack()[1][3] + "_" + str(counter[0]))
+    def return_name():
+        return print(inspect.stack()[1][1] + "_" + inspect.stack()[1][3] + "_line " + str(inspect.stack()[1][2]))
--- test.py
+++ test.py
@@ -4,5 +4,9 @@ from imhere.delogger.delogger import Delogger
 def example():
    Delogger.return_name()
 
+def pippo():
+   Delogger.return_name()
+
 if __name__ == "__main__":
     example()
+    pippo()
